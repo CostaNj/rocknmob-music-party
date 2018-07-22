5c3e54430d2bf68695029878e30f9644dab9b5f5
CostaNj <costa.n.work@yandex.ru>
2018-07-22 14:07:18 +0300

    start making soon page

--- src/route.js
+++ src/route.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {BrowserRouter, Route } from 'react-router-dom'
-import {Enter, Registration, Setlist} from './components'
+import {Enter, Registration, Setlist, Soon} from './components'
 
 export default class MyRouter extends Component {
     render() {
@@ -10,6 +10,7 @@ export default class MyRouter extends Component {
                    <Route exact path="/jam/registration" component={Registration}/>
                    <Route exact path="/jam" component={Enter}/>
                    <Route exact path="/jam/setlist" component={Setlist}/>
+                   <Route exact path="/jam/soon" component={Soon}/>
                </div>
            </BrowserRouter>
        )
