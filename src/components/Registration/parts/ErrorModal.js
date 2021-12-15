07dc43917a1d9ad7d1db3ced83ec9a851e83155a
CostaNj <costa.n.work@yandex.ru>
2021-12-16 02:16:52 +0300

    new error modal + images

--- src/components/Registration/parts/ErrorModal.js
+++ src/components/Registration/parts/ErrorModal.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import {Modal, ModalHeader, ModalBody, ModalFooter, Button} from 'reactstrap'
+import andrey from '../img/andrey.jpg'
+import ed from '../img/ed.PNG'
 
-export const ErrorModal = ({isOpen, errorMessage, closeErrorDialog}) => {
+export const ErrorModal = ({isOpen, errorMessage, errorType, closeErrorDialog, trackOfferLimit, participationLimit}) => {
 
     return (
         <Modal isOpen = {isOpen} centered>
@@ -11,28 +13,34 @@ export const ErrorModal = ({isOpen, errorMessage, closeErrorDialog}) => {
                 </div>
             </ModalHeader>
             <ModalBody>
-
-                <div>
-                    <ol>
-                        <li>
-                            Допускается участие в&nbsp;<span style={{color:'red'}}>3 треках</span>&nbsp;и возможность предложить
-                            &nbsp;<span style={{color:'red'}}>3 композиции</span>.
-                        </li>
-                        <li>
-                            В рамках одного трека можно участвовать на&nbsp;<span style={{color:'red'}}>нескольких позициях</span>&nbsp;
-                            (например гитара + вокал).
-                        </li>
-                        <li>
-                            Если в композиции нужен один вокалист, то можно занять сразу&nbsp;<span style={{color:'red'}}>2 ячейки.</span>&nbsp;
-                        </li>
-                    </ol>
-                    <p>
-                        По вопросам удаления предложенных треков обращайтесь к:&nbsp;
-                        <a style={{ cursor: 'pointer', color: '#333333', fontWeight: 'bold'}} href="https://vk.com/id2028637" target="_blank">
-                            Нюхтилину Константину
-                        </a>
-                    </p>
+                <div style={{display: 'flex', flexDirection:'row'}}>
+                    <div style={{flex: 4}}>
+                        <img style={{borderRadius: '60px', width:'100%', height: 'auto'}} src={errorType === 'instrumentsLimit' ? ed : andrey}/>
+                    </div>
+                    <div style={{flex: 6}}>
+                        <ol>
+                            <li>
+                                Общее кол-во треков, в которых можно поучаствовать:&nbsp;<span style={{color:'red'}}>{`${participationLimit}`}</span>.
+                            </li>
+                            <li>
+                                Общее кол-во треков, которые можно предложить:&nbsp;<span style={{color:'red'}}>{`${trackOfferLimit}`}</span>.
+                            </li>
+                            <li>
+                                В рамках одного трека можно участвовать на&nbsp;<span style={{color:'red'}}>нескольких позициях</span>&nbsp;
+                                (кроме одновременно несовместимых, например: <span style={{color:'red'}}>ударные и бас</span>).
+                            </li>
+                            <li>
+                                Если в композиции нужен один вокалист или гитарист, то можно занять сразу&nbsp;<span style={{color:'red'}}>2 ячейки.</span>&nbsp;
+                            </li>
+                        </ol>
+                    </div>
                 </div>
+                <p>
+                    По вопросам удаления предложенных треков обращайтесь к:&nbsp;
+                    <a style={{ cursor: 'pointer', color: '#333333', fontWeight: 'bold'}} href="https://vk.com/id2028637" target="_blank">
+                        Нюхтилину Константину
+                    </a>
+                </p>
             </ModalBody>
             <ModalFooter>
                 <Button color="secondary" onClick={closeErrorDialog}>Закрыть</Button>
