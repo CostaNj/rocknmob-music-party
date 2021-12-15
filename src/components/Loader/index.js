fb9cc949295c03473dab91c9caa328b2c79a7eca
CostaNj <costa.n.work@yandex.ru>
2021-12-16 02:17:39 +0300

    change loader color

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -5,7 +5,7 @@ export const Loader = ({loading}) => {
     return (
             <div style={{ width: '100px', margin: '0 auto', }}>
                 <ScaleLoader
-                    color={'rgba(13,22,223, 0.25)'}
+                    color={'rgba(222,13,23, 0.25)'}
                     loading={loading}
                     width={10}
                 />
