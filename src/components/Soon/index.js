09c49e0ef17e52d1583b29096278e92e4bdf4227
CostaNj <costa.n.work@yandex.ru>
2022-04-06 00:53:06 +0300

    fix soon page

--- src/components/Soon/index.js
+++ src/components/Soon/index.js
@@ -1,1 +1,35 @@
-export {Soon} from './Soon'
+import React from 'react'
+import YouTube from "react-youtube";
+
+import './soon.css'
+
+export const Soon = () => {
+
+  const handlePause = () => {
+    console.log('test')
+  }
+
+  const opts = {
+    width: '100%',
+    height: '100%',
+    playerVars: {
+      autoplay: 1,
+      controls: 0,
+      rel: 0,
+      showinfo: 0,
+      mute: 1,
+      loop: 1
+    },
+  }
+
+  return (
+    <div className='soon-container'>
+      <div className='soon-container-cloud'>
+        <div className='back-link'>
+          На данный момент нет активных мероприятий, перейти на&nbsp;<a href='/'>главную</a>
+        </div>
+      </div>
+      <YouTube videoId="Mof2D9bB5uU" opts={opts} containerClassName='soon-video' onPause={handlePause}  title='test' />
+    </div>
+  );
+}
